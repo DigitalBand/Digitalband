f991a3b08c4f3bd39b6bf7d1b9b0a3ee73d331af
easyacards <tim@easycards.org>
2013-01-20 00:29:15 +0400

    top product list

--- app/dao/impl/orm/slick/ProductRepository.scala
+++ app/dao/impl/orm/slick/ProductRepository.scala
@@ -1,7 +1,9 @@
 package dao.impl.orm.slick
 
 import common.RepositoryBase
-import models.ProductUnit
+import models.{ProductTable, ProductUnit}
+import scala.slick.driver.MySQLDriver.simple._
+import Database.threadLocalSession
 
 class ProductRepository extends RepositoryBase with dao.common.ProductRepository {
   def getList(): Seq[ProductUnit] = {
@@ -28,5 +30,20 @@ class ProductRepository extends RepositoryBase with dao.common.ProductRepository
         2, 0))
   }
 
-  def listMostVisited(count: Int) = ???
+  def listMostVisited(count: Int) = {
+    database withSession {
+     /* val productsQuery = Query(ProductTable)
+      productsQuery.filter(_.archived === false).sortBy(_.visitCounter.desc).take(8) */
+
+      val productsQuery = for {
+        p <- ProductTable if p.archived === false && p.price > 0.0
+      } yield (p.title, p.shortDescription, p.price, p.id, p.defaultImageId, p.visitCounter)
+      productsQuery.take(8).sortBy(_._6.desc).list.map {
+        product =>
+          product match {
+            case (title: String, descr: String, price: Double, id: Int, imageId: Int, visitCounter: Int) => ProductUnit(title, descr, price, id, imageId)
+          }
+      }
+    }
+  }
 }
--- app/models/ProductUnit.scala
+++ app/models/ProductUnit.scala
@@ -5,9 +5,17 @@ import Database.threadLocalSession
 
 case class ProductUnit(title:String, description: String, price: Double, id: Int, defaultImageId: Int)
 
-object ProductTable extends Table[(Int, String)]("products") {
-  def id = column[Int]("categoryId", O.PrimaryKey, O.AutoInc)
-  def path = column[String]("title")
-  def * = id ~ path
+object ProductTable extends Table[(Int, String, String, String, Double, java.sql.Date, Int, Int, Int, Boolean)]("products") {
+  def id = column[Int]("productId", O.PrimaryKey, O.AutoInc)
+  def title = column[String]("title")
+  def description = column[String]("description")
+  def shortDescription = column[String]("shortDescription")
+  def price = column[Double]("price")
+  def addedDate = column[java.sql.Date]("addedDate")
+  def brandId = column[Int]("brandId")
+  def defaultImageId = column[Int]("defaultImageId")
+  def visitCounter = column[Int]("visitCounter")
+  def archived = column[Boolean]("archived")
+  def * = id ~ title ~ description ~ shortDescription ~ price ~ addedDate ~ brandId ~ defaultImageId ~ visitCounter ~ archived
 }
 
