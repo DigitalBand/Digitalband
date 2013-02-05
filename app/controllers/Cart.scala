babd4acc6ae0b51f5782f0abc682356e0bce76d5
easyacards <tim@digitalband.ru>
2013-02-05 22:45:57 +0400

    action methods for cart controller

--- app/controllers/Cart.scala
+++ app/controllers/Cart.scala
@@ -5,5 +5,16 @@ import com.google.inject.Inject
 import dao.common.CartRepository
 
 class Cart @Inject()(val cartRepository: CartRepository) extends Controller{
-  def add(productId: Int, count: Int = 1) = ???
+  def add(productId: Int, count: Int = 1) = Action {
+    NotImplemented
+  }
+  def display = Action {
+    NotImplemented
+  }
+  def delete(productId: Int) = Action {
+    NotImplemented
+  }
+  def update(productId: Int, count: Int) = Action {
+    NotImplemented
+  }
 }
