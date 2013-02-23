b9c7aea09e054dca691233e8bbee9a56185e7c52
wassertim <tim@digitalband.ru>
2013-02-23 21:10:11 +0400

    cache-control

--- app/controllers/Image.scala
+++ app/controllers/Image.scala
@@ -6,11 +6,20 @@ import dao.common.ImageRepository
 import helpers.ImageCacher._
 import helpers.ImageHelper._
 
+
+
 class Image @Inject()(val imageRepository: ImageRepository) extends Controller {
-  def get(imageNumber: String, quality: Int, imageSize: String, fill: String) = Action {
-    val imageId: Int = getImageId(imageNumber)
-    sendCachedImage(imageId, getDimension(imageSize), checkQuality(quality), isCropped(fill)) {
-      imageRepository.get(imageId)
+  def get(imageNumber: String, quality: Int, imageSize: String, fill: String) =
+
+    Action {
+      val imageId: Int = getImageId(imageNumber)
+      sendCachedImage(imageId, getDimension(imageSize), checkQuality(quality), isCropped(fill)) {
+        imageRepository.get(imageId)
+      }.withHeaders(
+        CONTENT_TYPE -> "image/png",
+        CONTENT_DISPOSITION -> "inline",
+        CACHE_CONTROL -> "public, max-age=86400"
+      )
     }
-  }
+
 }
