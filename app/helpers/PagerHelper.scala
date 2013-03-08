e21fc0f4ff25901d37958fd69025c03715c4a019
wassertim <tim@digitalband.ru>
2013-03-08 23:37:12 +0400

    number comparison

--- app/helpers/PagerHelper.scala
+++ app/helpers/PagerHelper.scala
@@ -12,14 +12,14 @@ case class PagerHelper(val pageSize: Int, val totalCount: Int, val number: Int) 
     val pageCount = totalPageCount
     val startNumberCount = 6
     if (pageCount <= startNumberCount) pageCount
-    if (number != null && number != 0) {
+    if (number > 0) {
       val numberCount = (startNumberCount + number - 1)
       if (pageCount <= numberCount) pageCount else numberCount
     } else startNumberCount
   }
 
   def startPage: Int = {
-    if (number != null && number != 0) {
+    if (number > 0) {
       val midValue = (numbersLimit / 2) + 1
       if (number <= midValue) 1 else number - midValue
     } else 1
