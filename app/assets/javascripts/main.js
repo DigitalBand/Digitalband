254cfc661fa2d2bdd6bbe1d94ee530c2193d91ea
wassertim <tim@digitalband.ru>
2013-03-10 22:19:43 +0400

    require js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,14 +4,12 @@ require(["jquery",
     "lib/bootstrap/bootstrap-collapse",
     "lib/bootstrap/bootstrap-carousel"], function ($) {
 
-        $(document).ready(function () {
-            $("#myCarousel").carousel();
-            var settings = {
-                threshold: 100
-            };
-            $("img.lazy").lazyload(settings);
-            $("img.lazy-brand").lazyload(settings);
-        });
-
-
+    $(document).ready(function () {
+        $("#myCarousel").carousel();
+        var settings = {
+            threshold: 100
+        };
+        $("img.lazy").lazyload(settings);
+        $("img.lazy-brand").lazyload(settings);
+    });
 });
