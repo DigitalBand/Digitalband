8c12138b5987dbaf2c949463a5762df0c7b00414
Tim <tim@easycards.org>
2013-03-13 16:54:07 +0400

    admin pages

---
+++ app/controllers/Admin.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import com.google.inject.Inject
+import dao.common.UserRepository
+import controllers.common.ControllerBase
+import play.api.mvc.Action
+
+//should be secured
+class Admin @Inject()(implicit userRepository:UserRepository) extends ControllerBase {
+    def index = Action{
+      implicit request =>
+        Ok(views.html.Admin.index())
+    }
+}
