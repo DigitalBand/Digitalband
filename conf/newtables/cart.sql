57b8c82077596537c787448361ca1947b7e583e5
wassertim <tim@digitalband.ru>
2013-03-21 20:34:39 +0400

    conf

--- conf/newtables/cart.sql
+++ conf/newtables/cart.sql
@@ -1,11 +1,5 @@
-CREATE  TABLE `cart` (
-  `cartId` INT NOT NULL AUTO_INCREMENT ,
-  `createdDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ,
-  `userId` INT NOT NULL ,
-  PRIMARY KEY (`cartId`) ,
-  UNIQUE INDEX `cartcol_UNIQUE` (`userId` ASC) );
 
-  ALTER TABLE `dbs`.`shopping_items` ADD COLUMN `cartId` INT NULL  AFTER `unitPrice` ;
+
 
 ALTER TABLE `shopping_items` CHANGE COLUMN `userId` `userId` INT(11) NOT NULL DEFAULT 0  ;
 ALTER TABLE `shopping_items` CHANGE COLUMN `unitPrice` `unitPrice` DOUBLE NULL  ;
