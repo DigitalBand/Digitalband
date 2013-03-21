b7801d70c48a27c7915bc986d2708b64789c204f
wassertim <tim@digitalband.ru>
2013-03-21 22:04:11 +0400

    async email

--- app/helpers/EmailHelper.scala
+++ app/helpers/EmailHelper.scala
@@ -5,26 +5,27 @@ import com.typesafe.plugin._
 import play.api.Play.current
 import dao.common.UserRepository
 import play.api.i18n.Messages
+import play.api.libs.concurrent.Akka
 
 class EmailHelper(implicit userRepository: UserRepository) {
   def systemEmail = userRepository.getSystemEmail
   def adminEmails = userRepository.getAdminEmails
 
-  def orderConfirmed(comment: String, order: OrderInfo)(implicit request: play.api.mvc.Request[Any]) = {
+  def orderConfirmed(comment: String, order: OrderInfo)(implicit request: play.api.mvc.Request[Any]) = Akka.future {
     val mail = use[MailerPlugin].email
     mail.setSubject(s"Подтверждение заказа №${order.id}")
     mail.addFrom(systemEmail)
     mail.addRecipient(order.deliveryInfo.email)
-    mail.send(comment)
+    mail.sendHtml(comment)
   }
-  def orderCanceled(comment: String, order: OrderInfo)(implicit request: play.api.mvc.Request[Any]) = {
+  def orderCanceled(comment: String, order: OrderInfo)(implicit request: play.api.mvc.Request[Any]) = Akka.future {
     val mail = use[MailerPlugin].email
     mail.setSubject(s"Информация по заказу №${order.id}")
     mail.addFrom(systemEmail)
     mail.addRecipient(order.deliveryInfo.email)
-    mail.send(comment)
+    mail.sendHtml(comment)
   }
-  def sendFeedback(message: ContactEntity)(implicit request: play.api.mvc.Request[Any]) = {
+  def sendFeedback(message: ContactEntity)(implicit request: play.api.mvc.Request[Any]) = Akka.future {
     val mail: MailerAPI = use[MailerPlugin].email
     mail.setSubject(Messages("emailhelper.sendfeedback.subject"))
     val adminEmails = userRepository.getAdminEmails
@@ -34,7 +35,7 @@ class EmailHelper(implicit userRepository: UserRepository) {
     mail.sendHtml(views.html.emails.plain.contact.feedback(message).body)
   }
 
-  def orderConfirmation(order: OrderInfo)(implicit request: play.api.mvc.Request[Any]) = {
+  def orderConfirmation(order: OrderInfo)(implicit request: play.api.mvc.Request[Any]) = Akka.future {
         val deliveryInfo = order.deliveryInfo
         sendToClient(systemEmail, deliveryInfo.email)
         sendToAdmins(adminEmails, deliveryInfo.email, systemEmail)
@@ -43,7 +44,7 @@ class EmailHelper(implicit userRepository: UserRepository) {
           mail.setSubject(Messages("emailhelper.orderconfirmation.subject"))
           mail.addRecipient(to)
           mail.addFrom(from)
-          mail.send(views.html.emails.plain.order.confirmation(order).body)
+          mail.sendHtml(views.html.emails.plain.order.confirmation(order).body)
         }
         def sendToAdmins(adminEmails: Seq[String], userEmail: String, systemEmail: String) = {
           val mail: MailerAPI = use[MailerPlugin].email
@@ -51,7 +52,7 @@ class EmailHelper(implicit userRepository: UserRepository) {
           mail.addFrom(systemEmail)
           mail.setReplyTo(userEmail)
           adminEmails.map(email => mail.addRecipient(email))
-          mail.send(views.html.emails.plain.order.adminConfirmation(order).body)
+          mail.sendHtml(views.html.emails.plain.order.adminConfirmation(order).body)
         }
 
   }
