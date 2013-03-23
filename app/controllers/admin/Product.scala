e28f49992868e2304ee95aed637689db5879e5ea
wassertim <tim@digitalband.ru>
2013-03-23 14:59:21 +0400

    product details

--- app/controllers/admin/Product.scala
+++ app/controllers/admin/Product.scala
@@ -1,5 +1,36 @@
 package controllers.admin
 
-class Product {
+import com.google.inject.Inject
+import controllers.common.ControllerBase
+import dao.common.UserRepository
+import helpers.Secured
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.Messages
+import models.ProductEntity
 
+
+class Product @Inject()(implicit userRepository: UserRepository) extends ControllerBase with Secured {
+
+ /* val productForm = Form(
+    mapping(
+      "title" -> nonEmptyText,
+      "description" -> nonEmptyText,
+      "shortDescription" -> nonEmptyText,
+      "imageId" -> number,
+      "brandId" -> number,
+    )(ProductEntity)
+  )*/
+
+  def create = withAdmin {
+    user =>
+      implicit request =>
+        NotImplemented
+  }
+
+  def save = withAdmin {
+    user =>
+      implicit request =>
+        NotImplemented
+  }
 }
