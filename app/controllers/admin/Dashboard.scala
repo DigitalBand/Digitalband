a47bb86e686ca7cf0be96e25dc17be4da57e697a
Tim <tim@easycards.org>
2013-03-25 10:26:55 +0400

    chart for orders

--- app/controllers/admin/Dashboard.scala
+++ app/controllers/admin/Dashboard.scala
@@ -1,15 +1,26 @@
 package controllers.admin
 
 import com.google.inject.Inject
-import dao.common.UserRepository
+import dao.common.{OrderRepository, UserRepository}
 import controllers.common.ControllerBase
-import play.api.mvc.Action
 import helpers.Secured
+import play.api.libs.json.Json
+import play.api.i18n.Messages
 
-//should be secured
-class Dashboard @Inject()(implicit userRepository: UserRepository) extends ControllerBase with Secured{
-  def index = withAdmin { implicit user =>
-    implicit request =>
-      Ok(views.html.Admin.index())
+class Dashboard @Inject()(implicit userRepository: UserRepository, orderRepository: OrderRepository) extends ControllerBase with Secured {
+  def index = withAdmin {
+    implicit user =>
+      implicit request =>
+        val counters = orderRepository.getCounters
+        val countersJs = Json.toJson(
+          counters.map {
+            counter =>
+              Json.obj (
+                "name" -> Messages("order.list.ui.button." + counter._1 + ".text"),
+                "y" -> Json.toJson(counter._2),
+                "color" -> Messages("order.statuses.colors." + counter._1)
+              )
+          })
+        Ok(views.html.Admin.index(countersJs))
   }
 }
