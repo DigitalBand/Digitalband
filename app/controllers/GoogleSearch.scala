d49b07ca18ab92c833f8aeed50f8ec734c495a1c
Tim <tim@easycards.org>
2013-03-26 17:27:01 +0400

    google image

--- app/controllers/GoogleSearch.scala
+++ app/controllers/GoogleSearch.scala
@@ -17,8 +17,8 @@ class GoogleSearch @Inject()(implicit userRepository: UserRepository) extends Co
     user => request => Async {
       val num = 10
       val offset = (pageNumber - 1) * num + 1
-      val key = "AIzaSyDByv-9nh9XS9RpLSMUgqj7z6iUejzG8nY"
-      val cx = "006243113112137199630:y3v5ecyacm8"
+      val key = "AIzaSyAHyhiLNqUOfhcf362AoEEPl6s65UhzbMg"//"AIzaSyDByv-9nh9XS9RpLSMUgqj7z6iUejzG8nY"
+      val cx = "009346898408990037447:evnb2u_eytw"//"006243113112137199630:y3v5ecyacm8"
       val searchEncoded = java.net.URLEncoder.encode(search, "UTF-8")
       val params = s"key=$key&cx=$cx&q=$searchEncoded&searchType=image&start=$offset&num=$num"
       val url = s"https://www.googleapis.com/customsearch/v1?$params"
@@ -38,7 +38,7 @@ class GoogleSearch @Inject()(implicit userRepository: UserRepository) extends Co
             )
         }
         val imageList = new ListPage(pageNumber, images, totalResults)
-        Ok(views.html.Admin.GoogleImages.list(imageList, search))
+        Ok(views.html.Admin.GoogleImages.list(imageList, search)).withHeaders(CACHE_CONTROL -> "public, max-age=86400")
       }
     }
   }
