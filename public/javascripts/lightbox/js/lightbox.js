f9aa2239cefd4c67a3f3ae39bc58e2d08cd01948
wassertim <tim@digitalband.ru>
2013-03-30 10:44:57 +0400

    lightbox paths

--- public/javascripts/lightbox/js/lightbox.js
+++ public/javascripts/lightbox/js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = '../assets/javascripts/lightbox/images/loading.gif';
-      this.fileCloseImage = '../assets/javascripts/lightbox/images/close.png';
+      this.fileLoadingImage = '/assets/javascripts/lightbox/images/loading.gif';
+      this.fileCloseImage = '/assets/javascripts/lightbox/images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Фото";
