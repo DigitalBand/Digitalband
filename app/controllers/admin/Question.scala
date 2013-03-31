b15abb98989b6ec25f6e2246c413227e5c1e4797
wassertim <tim@digitalband.ru>
2013-03-31 16:13:38 +0400

    app

--- app/controllers/admin/Question.scala
+++ app/controllers/admin/Question.scala
@@ -9,6 +9,7 @@ import play.api.data.Forms._
 import play.api.libs.concurrent.Akka
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.duration._
+import play.api.Play.current
 
 class Question @Inject()(implicit userRepository: UserRepository, productRepository: ProductRepository, questionRepository: QuestionRepository) extends ControllerBase with Secured {
   val commentForm = Form("comment" -> text)
