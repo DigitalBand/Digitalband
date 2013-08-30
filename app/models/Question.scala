a2f75925c31862cc9277d4f61f7c70a5280a878d
Tim <tim@digitalband.ru>
2013-08-30 15:17:24 +0400

    question table

--- app/models/Question.scala
+++ app/models/Question.scala
@@ -1,3 +1,3 @@
 package models
 
-class Question(val id: Int, val productId: Int, val productTitle: String, val email: String, questionType: String)
+class Question(val id: Int, val productId: Int, val productTitle: String, val email: String, val questionType: String)
