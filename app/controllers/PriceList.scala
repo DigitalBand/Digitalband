4bdb4a620b6278f7655b6a82e2323117b9169c7f
wassertim <tim@digitalband.ru>
2014-09-04 21:59:47 +0200

    yandex prices fix

--- app/controllers/PriceList.scala
+++ app/controllers/PriceList.scala
@@ -9,26 +9,24 @@ import play.api.mvc.Action
 
 class PriceList @Inject()(implicit userRepository: UserRepository, shopRepository: ShopRepository, productRepository: ProductRepository, categoryRepository: CategoryRepository) extends ControllerBase {
   def forYandex = Action {
-
-    val w = new java.io.StringWriter()
-    val yandexShopInfo = shopRepository.getYandexShopInfo
-    val products = productRepository.listAll
-    val categories = categoryRepository.listAll
-    val fmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm")
-    val getUrl: Int => String = { id => routes.Product.display(id).url}
-    val getPictureUrl: Int => String = { id => routes.Image.get(id.toString + ".jpg", 90, "150x150", "full").url}
-    val yandex = views.html.PriceList.forYandex(
-      products,
-      categories,
-      fmt.print(DateTime.now().withZone(DateTimeZone.forID("Europe/Moscow"))),
-      yandexShopInfo,
-      getUrl,
-      getPictureUrl
-    ).toString()
-
-
-    //xml.XML.write(w, yandex, "UTF-8", xmlDecl = true, doctype =
-    //xml.dtd.DocType("yml_catalog", xml.dtd.SystemID("shops.dtd"), Nil))
-    Ok(yandex.trim).withHeaders(CONTENT_TYPE -> "text/xml")
+    implicit request =>
+      val w = new java.io.StringWriter()
+      val yandexShopInfo = shopRepository.getYandexShopInfo
+      val products = productRepository.listAll
+      val categories = categoryRepository.listAll
+      val fmt = DateTimeFormat.forPattern("YYYY-MM-dd HH:mm")
+      val getUrl: Int => String = { id => routes.Product.display(id).url }
+      val getPictureUrl: Int => String = { id => routes.Image.get(id.toString + ".jpg", 90, "150x150", "full").url}
+      val yandex = views.html.PriceList.forYandex(
+        products,
+        categories,
+        fmt.print(DateTime.now().withZone(DateTimeZone.forID("Europe/Moscow"))),
+        yandexShopInfo,
+        getUrl,
+        getPictureUrl,
+        request.host
+      ).toString()
+      Ok(yandex.trim).withHeaders(CONTENT_TYPE -> "text/xml")
   }
+
 }
