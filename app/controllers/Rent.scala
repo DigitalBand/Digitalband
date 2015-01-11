764e12d9f1384cfd3cb692e5fb973f8cb9e8258c
wassertim <tim@digitalband.ru>
2015-01-11 15:12:45 +0100

    Task #93 @1h add form

--- app/controllers/Rent.scala
+++ app/controllers/Rent.scala
@@ -5,21 +5,56 @@ import dao.common.ProductRepository
 import dao.impl.orm.slick.UserRepository
 import com.google.inject.Inject
 import helpers.withUser
+import play.api.data.Form
+import play.api.data.Forms._
+
+case class RentRequest(
+  quantity: Int,
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  city: String,
+  street: String,
+  notes: Option[String]
+)
 
 class Rent @Inject()(implicit ur: UserRepository, productRepository: ProductRepository) extends ControllerBase{
+
+  val rentForm = Form(mapping(
+      "quantity" -> number,
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText,
+      "email" -> email,
+      "phone" -> nonEmptyText,
+      "city" -> nonEmptyText,
+      "street" -> nonEmptyText,
+      "notes" -> optional(text)
+    )(RentRequest.apply)(RentRequest.unapply)
+  )
 
   def requestRent(productId: Int) = withUser {
     implicit user =>
       implicit request =>
         val product = productRepository.get(productId)
 
-        Ok(views.html.Rent.requestRent(product))
+        Ok(views.html.Rent.requestRent(product, rentForm))
   }
 
   def postRequest(productId: Int) = withUser {
     implicit user =>
       implicit request =>
+      rentForm.bindFromRequest.fold(
+        formWithErrors => {
+          val product = productRepository.get(productId)
+          BadRequest(views.html.Rent.requestRent(product, formWithErrors))
+        },
+        rentRequest => {
+          val product = productRepository.get(productId)
+          //RentEmailHelper.notifyNewRent(product, rentRequest)
+          Ok("good")
+        }
+      )
 
-      Ok("")
   }
 }
