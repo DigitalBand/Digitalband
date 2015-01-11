4fbc91237ab5a33a0fcfa36cfe1baa8fc20d6290
wassertim <tim@digitalband.ru>
2015-01-11 14:11:11 +0100

    Task #93 @20m add request rent form fields

--- app/controllers/Rent.scala
+++ app/controllers/Rent.scala
@@ -7,10 +7,19 @@ import com.google.inject.Inject
 import helpers.withUser
 
 class Rent @Inject()(implicit ur: UserRepository, productRepository: ProductRepository) extends ControllerBase{
+
   def requestRent(productId: Int) = withUser {
     implicit user =>
       implicit request =>
         val product = productRepository.get(productId)
+
         Ok(views.html.Rent.requestRent(product))
+  }
+
+  def postRequest(productId: Int) = withUser {
+    implicit user =>
+      implicit request =>
+
+      Ok("")
   }
 }
