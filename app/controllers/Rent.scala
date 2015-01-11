ec3d574d704c01e322b004c77bda5b3dd9b36044
wassertim <tim@digitalband.ru>
2015-01-11 13:53:58 +0100

    Task #93 @1h add route, view and link for a rent feature

---
+++ app/controllers/Rent.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import controllers.common.ControllerBase
+import dao.common.ProductRepository
+import dao.impl.orm.slick.UserRepository
+import com.google.inject.Inject
+import helpers.withUser
+
+class Rent @Inject()(implicit ur: UserRepository, productRepository: ProductRepository) extends ControllerBase{
+  def requestRent(productId: Int) = withUser {
+    implicit user =>
+      implicit request =>
+        val product = productRepository.get(productId)
+        Ok(views.html.Rent.requestRent(product))
+  }
+}
