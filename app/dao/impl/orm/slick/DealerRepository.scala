26a97e70dc873b69b782bd04aeff8852eb7a0325
Timofey Tkachenko <tim@StarGate.local>
2015-09-20 17:01:43 +0200

    fix dealers repository

--- app/dao/impl/orm/slick/DealerRepository.scala
+++ app/dao/impl/orm/slick/DealerRepository.scala
@@ -1,18 +1,18 @@
 package dao.impl.orm.slick
 
-import slick.driver.JdbcDriver.backend.Database
-import Database.dynamicSession
+import slick.driver.JdbcDriver.api._
 import slick.jdbc.{StaticQuery => Q, GetResult}
-import Q.interpolation
 import models.DealerInfo
 import dao.impl.orm.slick.common.RepositoryBase
 
+import scala.concurrent.Future
+
 
 class DealerRepository extends RepositoryBase with dao.common.DealerRepository {
-  override def list: Seq[DealerInfo] = database withDynSession {
+  override def list: Future[Seq[DealerInfo]] = usingDB {
     implicit val getDealers = GetResult(r => DealerInfo(r.<<, r.<<))
     sql"""
       SELECT id, title FROM dealers;
-    """.as[DealerInfo].list
+    """.as[DealerInfo]
   }
 }
